744003f7212ed5b3f454803da96432e430282f3f
root <1207146524@qq.com>
2024-03-01 16:21:33 +0800

    add singleflight

--- EasyCache/easycache.go
+++ EasyCache/easycache.go
@@ -1,6 +1,7 @@
 package EasyCache
 
 import (
+	"EasyCache/singleflight"
 	"fmt"
 	"log"
 	"sync"
@@ -22,6 +23,7 @@ type Group struct {
 	getter    Getter // 缓存未击中的回调
 	mainCache cache
 	peers     PeerPicker
+	loader    *singleflight.Group
 }
 
 var (
@@ -39,6 +41,7 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
@@ -86,15 +89,24 @@ func (g *Group) RegisterPeers(peers PeerPicker) {
 }
 
 func (g *Group) load(key string) (value ByteView, err error) {
-	if g.peers != nil {
-		if peer, ok := g.peers.PickPeer(key); ok {
-			if value, err = g.getFromPeer(peer, key); err == nil {
-				return value, nil
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[EasyCache] Failed to get from peer", err)
 			}
-			log.Println("[EasyCache] Failed to get from peer", err)
 		}
+
+		return g.getLocally(key)
+	})
+
+	if err == nil {
+		return viewi.(ByteView), nil
 	}
-	return g.getLocally(key)
+
+	return
 }
 
 func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
